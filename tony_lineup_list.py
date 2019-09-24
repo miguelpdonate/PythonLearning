6fbcbc09d8b17f0338828d8c23fea789c4eccaf1
Miguel <36984264+miguelpdonate@users.noreply.github.com>
2019-09-24 20:48:35 +0200

    initial

---
+++ tony_lineup_list.py
@@ -0,0 +1,12 @@
+
+kangaroos_lineup = ['Nathan', 'Cedric', 'Peter', 'Juan', 'Guillaume', 'Juan Carlos', 'Ryouga', 'Miguel', 'Gabriel']
+
+# for player in kangaroos_lineup:
+#     print(player)
+
+idx = 0
+
+while idx < len(kangaroos_lineup):
+    print(f'{idx + 1}. {kangaroos_lineup[idx]}')
+    idx += 1
+
